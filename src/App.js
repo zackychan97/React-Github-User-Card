119aeaee73baebf598bb608afa8c8d4669ac0cf3
Zack Bumpous <upwork.ztb@gmail.com>
2019-10-30 09:49:56 -0500

    Adds code to app js file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 //adding comment for pull request
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserCard from "./components/UserCard";
+import SearchAppBar from "./components/Nav";
+
+
+class App extends React.Component {
+  state = {
+    user: [],
+    followers: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://api.github.com/users/seanaleid`)
+      .then(response =>{
+        console.log(response)
+        console.log(response.data.followers_url)
+        this.setState({
+          user: response.data
+        });
+        console.log(this.state);
+      })
+      .catch(err =>{
+        console.log(`error`)
+      })
+    axios
+      .get(`https://api.github.com/users/seanaleid/followers`)
+      .then(res => {
+        console.log(res)
+        this.setState({
+          followers: res.data
+        })
+      })
+      .catch(err => {
+        console.log(`error`)
+      })
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <SearchAppBar/>
+        <div className="cards-grid">
+          <UserCard key={this.state.user}  user={this.state.user}/>
+          {this.state.followers.map(props =>(
+            <UserCard key={props.id} user={props} />
+          ))}
+        </div>
+      </div>
+    );
+  };
 }
 
 export default App;
